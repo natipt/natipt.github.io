7592eb9f8ca7b6b47fb30e27e5e3e51622b8fc8b
natipi <nataliap@mit.edu>
2025-02-15 16:49:48 -0500

    add randomized header image (its a bit glitchy tho)

--- script.js
+++ script.js
@@ -1,52 +1,18 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const backgroundContainer = document.querySelector(".background-container");
-//     const backgroundCover = document.querySelector(".background-cover");
-//     const listItems = document.querySelectorAll(".hidden-gallery");
-//     const body = document.body;
+document.addEventListener("DOMContentLoaded", function() {
+    // Array of image paths
+    const images = [
+        "media/IMG_3931.png",
+        "media/IMG_6998.JPG",
+        "media/IMG_6161.png",
+        "media/IMG_4428.PNG",
+        "media/IMG_3955 2.png"
+    ];
 
-//     listItems.forEach(item => {
-//         item.addEventListener("mouseenter", () => {
-//             console.log("hovered");
-//             const images = JSON.parse(item.getAttribute("data-images"));
-            
-//             // Clear previous tiles
-//             backgroundContainer.innerHTML = "";
+    // Select a random image
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
 
-//             // Add class to body
-//             body.classList.add("background-gallery-active");
-//             body.classList.add("text-white");
-            
-//             // Make background visible
-//             backgroundContainer.style.opacity = "1";
-//             backgroundCover.style.opacity="0.1";
+    // Set the image source
+    document.querySelector(".header-image").src = randomImage;
+});
 
-//             images.forEach((imgSrc, index) => {
-//                 const imgElement = document.createElement("img");
-//                 imgElement.src = imgSrc;
-//                 imgElement.classList.add("background-gallery-image");
-
-//                 // Delay the fade-in effect for a staggered appearance
-//                 // Stagger effect every 200ms
-//                 // setTimeout(() => {
-//                 imgElement.classList.add("visible");
-//                 // }, index * 200); 
-
-//                 backgroundContainer.appendChild(imgElement);
-//             });
-//         });
-
-//         item.addEventListener("mouseleave", () => {
-//             backgroundContainer.style.opacity = "0";
-//             backgroundCover.style.opacity="1";
-//             body.classList.remove("text-white");
-
-//             // Remove class from body
-//             body.classList.remove("dark");
-//             setTimeout(() => {
-//                 backgroundContainer.innerHTML = ""; // Remove images after fading out
-//                 // Remove class from body
-//                 body.classList.remove("background-gallery-active");
-//             }, 500);
-//         });
-//     });
-// });
