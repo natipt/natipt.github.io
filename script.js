8d78d3f9b4ac001b66ae3d3a2a9c890358340345
natipi <natipt@users.noreply.github.com>
2025-03-01 15:28:10 -0500

    fixed mobile display including size of list items and neglecting hover effects of image gallery on screens less than 767px wide

--- script.js
+++ script.js
@@ -1,44 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // // Array of image paths
-    // const images = [
-    //     "media/IMG_3931.png",
-    //     "media/IMG_6998.JPG",
-    //     "media/IMG_6161.png",
-    //     "media/IMG_4428.PNG",
-    //     "media/IMG_3955 2.png"
-    // ];
-
-    // // Select a random image
-    // const randomIndex = Math.floor(Math.random() * images.length);
-    // const randomImage = images[randomIndex];
-
-    // // Set the image source
-    // document.querySelector(".header-image").src = randomImage;
-
     const imageGallery = document.querySelector(".flexbox-image-gallery");
     const listItems = document.querySelectorAll(".hidden-galleries li");
     const body = document.body;
+    
 
     listItems.forEach(item => {
         item.addEventListener("mouseenter", () => {
-            console.log("hovered");
-            const images = JSON.parse(item.getAttribute("data-images"));
-            console.log(images)
-            body.classList.add("no-backgrounds");
-
-            images.forEach((imgId, index) => {
-                const imgElement = document.getElementById(imgId);
-                console.log("got image by id?")
-                console.log(imgId)
-                console.log(imgElement.classList)
-                imgElement.classList.add("visible");
-            });
+            var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+            console.log(vw)
+            if (vw > 767) {
+                const images = JSON.parse(item.getAttribute("data-images"));
+                body.classList.add("no-backgrounds");
+    
+                images.forEach((imgId, index) => {
+                    const imgElement = document.getElementById(imgId);
+                    imgElement.classList.add("visible");
+                });
+            }
         });
 
         item.addEventListener("mouseleave", () => {
             // Fade out images
             const images = document.querySelectorAll(".flexbox-image-gallery .visible");
-            console.log("unhovered")
             images.forEach((img, index) => {
                 setTimeout(() => {
                     img.classList.remove("visible"); // Start fade-out
@@ -48,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // display data source in menu
     const allItems = document.querySelectorAll(".list ul li");
     const menu = document.getElementById("menu");
     const menuspan = menu.querySelector(".source span");
