2bc93a0e17feac8d021dbda7644dc0838b5724ad
natipt <natipt@users.noreply.github.com>
2025-05-20 21:18:07 -0400

    add zhongwen reader and styles for card gallery entries

--- script.js
+++ script.js
@@ -148,3 +148,42 @@ document.addEventListener("DOMContentLoaded", function() {
   darkBtn.addEventListener("click", () => setTheme("dark"));
 });
 
+
+
+// SCROLL REVEAL
+// Reference: https://css-tricks.com/scroll-triggered-animation-vanilla-javascript/
+function scrollTrigger(selector, options = {}){
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+        addObserver(el, options)
+    })
+}
+
+function addObserver(el, options){
+    if(!('IntersectionObserver' in window)){
+        if(options.cb){
+            options.cb(el)
+        }else{
+            entry.target.classList.add('active')
+        }
+        return
+    }
+    let observer = new IntersectionObserver((entries, observer) => { //this takes a callback function which receives two arguments: the elemts list and the observer instance
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                if(options.cb){
+                    options.cb(el)
+                }else{
+                    entry.target.classList.add('active')
+                }
+                observer.unobserve(entry.target)
+            }
+        })
+    }, options)
+    observer.observe(el)
+}
+
+scrollTrigger('.scroll-reveal', {
+    rootMargin: '0px', // shift the boundaries of the viewport that we consider for triggering the reveal
+})
